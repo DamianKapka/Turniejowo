011e85085c93421b61577e339d2f2a042df0b6c6
Damian Kapka <damiankapka@live.com>
2019-03-23 02:36:01 +0100

    Entity installed; Database created

---
+++ Turniejowo.API/Models/TurniejowoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Turniejowo.API.Models
+{
+    public class TurniejowoDbContext : DbContext
+    {
+        public TurniejowoDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
--- Turniejowo.API/Startup.cs
+++ Turniejowo.API/Startup.cs
@@ -5,30 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Turniejowo.API.Models;
 
 namespace Turniejowo.API
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TurniejowoDbContext>(opt =>
+                opt.UseSqlServer(_config.GetConnectionString("TurniejowoDB")));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
