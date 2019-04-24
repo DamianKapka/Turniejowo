8f56c7162545489ebc41b11197f53d96389dd435
Damian Kapka <damiankapka@live.com>
2019-04-24 23:47:47 +0200

    UserController with GetById and Create

---
+++ Turniejowo.API/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Turniejowo.API.Models;
+using Turniejowo.API.Models.Repositories;
+using Turniejowo.API.Models.UnitOfWork;
+
+namespace Turniejowo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var user = await _userRepository.FindSingle(x => x.UserId == id);
+
+                if (user == null)
+                {
+                    return BadRequest("User does no exist");
+                }
+
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (await _userRepository.FindSingle(x => x.Email == user.Email) != null)
+                {
+                    return Conflict("Account with that e-mail already exists in database.");
+                }
+
+                _userRepository.Add(user);
+                await _unitOfWork.CompleteAsync();
+
+                return CreatedAtAction("GetById", new {id = user.UserId}, user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
