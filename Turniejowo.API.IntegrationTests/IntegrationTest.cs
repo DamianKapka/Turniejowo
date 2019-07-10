feaa712aebdcaf67ed6a156f72a8932c821aaa08
Damian Kapka <damiankapka@live.com>
2019-07-10 23:33:57 +0200

    IntegrationTests project with abstract IntegrationTest class

---
+++ Turniejowo.API.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Turniejowo.API.IntegrationTests
+{
+    public abstract class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
