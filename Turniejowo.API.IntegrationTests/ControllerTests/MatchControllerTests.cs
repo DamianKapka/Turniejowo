034b910170df965d38f651cc47f2b0b45832455a
Damian Kapka <damiankapka@live.com>
2019-07-11 20:22:57 +0200

    Getting token for IntegrationTests + MatchController GetAll tests

---
+++ Turniejowo.API.IntegrationTests/ControllerTests/MatchControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Turniejowo.API.Models;
+using Xunit;
+
+namespace Turniejowo.API.IntegrationTests.ControllerTests
+{
+    
+    public class MatchControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutToken_Returns401()
+        {
+            var response = await TestClient.GetAsync("api/match");
+
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAll_WithoutPosts_Returns404()
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            //Act
+            var response = await TestClient.GetAsync("api/match");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NotFound,response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAll_WithPosts_Returns200()
+        {
+            //Arrange 
+            await AuthenticateAsync();
+
+            //Act
+
+            //Assert
+        }
+    }
+}
--- Turniejowo.API.IntegrationTests/IntegrationTest.cs
+++ Turniejowo.API.IntegrationTests/IntegrationTest.cs
@@ -1,19 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Turniejowo.API.Contracts.Requests;
+using Turniejowo.API.Models;
+using Turniejowo.API.Repositories;
 
 namespace Turniejowo.API.IntegrationTests
 {
-    public abstract class IntegrationTest
+    public class IntegrationTest
     {
         protected readonly HttpClient TestClient;
+        protected bool IsTestUserRegistered;
 
         protected IntegrationTest()
         {
-            var appFactory = new WebApplicationFactory<Startup>();
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder => { builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(TurniejowoDbContext));
+                        services.AddDbContext<TurniejowoDbContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");  
+
+                        });
+                    });
+            });
             TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer",await GetJwtAsync());
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            if (!IsTestUserRegistered)
+            {
+                var lolo = await TestClient.PostAsJsonAsync("api/user", new User()
+                {
+                    Email = "test@test.com",
+                    FullName = "test test",
+                    Password = "testtest",
+                    Phone = "123456789",
+                });
+
+                IsTestUserRegistered = true; 
+            }
+
+            var response = await TestClient.PostAsJsonAsync("api/user/authenticate", new Credentials()
+            {
+                Login = "test@test.com",
+                Password = "testtest"
+            });
+
+            var authResponse = await response.Content.ReadAsAsync<User>();
+
+            return authResponse.Token;
+        }
+
+        protected async Task InsertDummyData()
+        {
+
         }
     }
 }
