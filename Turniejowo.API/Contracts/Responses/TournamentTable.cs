2e305f09558bc35917aa35e6190b4e94e9db51a0
Damian Kapka <Damian Kapka@327-DKAPKA>
2019-07-18 14:56:04 +0200

    TournamentTable

---
+++ Turniejowo.API/Contracts/Responses/TournamentTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Turniejowo.API.Contracts.Responses
+{
+    public class TournamentTable
+    {
+    }
+}
