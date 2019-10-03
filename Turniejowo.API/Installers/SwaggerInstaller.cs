baddf6a6bda531fc006023201197fb91bef82b2e
Damian Kapka <damiankapka@live.com>
2019-10-03 20:09:08 +0200

    v 1.4

--- Turniejowo.API/Installers/SwaggerInstaller.cs
+++ Turniejowo.API/Installers/SwaggerInstaller.cs
@@ -16,7 +16,7 @@ namespace Turniejowo.API.Installers
                 x.SwaggerDoc("turniejowo", new Swashbuckle.AspNetCore.Swagger.Info
                 {
                     Title = "Turniejowo API",
-                    Version = "v1.39"
+                    Version = "v1.40"
                 });
             });
         }
