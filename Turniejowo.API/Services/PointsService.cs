8ca26c0d8b3759a811dfcba44e3a89d186d093dc
Damian Kapka <damiankapka@live.com>
2019-10-09 22:02:50 +0200

    Implementation of GET methods in PointsService

--- Turniejowo.API/Services/PointsService.cs
+++ Turniejowo.API/Services/PointsService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Turniejowo.API.Exceptions;
 using Turniejowo.API.Models;
 using Turniejowo.API.Repositories;
 using Turniejowo.API.UnitOfWork;
@@ -11,40 +12,61 @@ namespace Turniejowo.API.Services
     public class PointsService : IPointsService
     {
         private readonly IPointsRepository pointsRepository;
+        private readonly IMatchRepository matchRepository;
+        private readonly ITournamentRepository tournamentRepository;
+        private readonly IPlayerRepository playerRepository;
         private readonly IUnitOfWork unitOfWork;
 
-        public PointsService(PointsRepository pointsRepository,IUnitOfWork unitOfWork)
+        public PointsService(PointsRepository pointsRepository,IUnitOfWork unitOfWork, IMatchRepository matchRepository, ITournamentRepository tournamentRepository, IPlayerRepository playerRepository)
         {
             this.pointsRepository = pointsRepository;
             this.unitOfWork = unitOfWork;
+            this.matchRepository = matchRepository;
+            this.tournamentRepository = tournamentRepository;
+            this.playerRepository = playerRepository;
         }
 
-        public Task<ICollection<Points>> GetPointsForMatch(int matchId)
+        public async Task<ICollection<Points>> GetPointsForMatch(int matchId)
+        {
+            var match = await matchRepository.FindSingleAsync(m => m.MatchId == matchId) 
+                        ?? throw new NotFoundInDatabaseException();
+
+            var points = await pointsRepository.FindAsync(p => p.MatchId == matchId, new string[] { "Tournament", "Player", "Match" });
+
+            return points;
+        }
+
+        public async Task<ICollection<Points>> GetPointsForTournament(int tournamentId)
+        {
+            var tournament = await tournamentRepository.FindSingleAsync(t => t.TournamentId == tournamentId) 
+                             ?? throw new NotFoundInDatabaseException();
+
+            var points = await pointsRepository.FindAsync(p => p.TournamentId == tournamentId,new string[]{"Tournament","Player","Match"});
+
+            return points;
+        }
+
+        public async Task<ICollection<Points>> GetPointsForPlayer(int playerId)
+        {
+            var player = await playerRepository.FindSingleAsync(p => p.PlayerId == playerId)
+                         ?? throw new NotFoundInDatabaseException();
+
+            var points = await pointsRepository.FindAsync(p => p.PlayerId == playerId, new string[] { "Tournament", "Player", "Match" });
+
+            return points;
+        }
+
+        public async Task AddPointsForMatchAsync(ICollection<Points> points)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ICollection<Points>> GetPointsForTournament(int tournamentId)
+        public async Task EditPointsForMatchAsync(ICollection<Points> points)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ICollection<Points>> GetPointsForPlayer(int playerId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task AddPointsForMatchAsync(ICollection<Points> points)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task EditPointsForMatchAsync(ICollection<Points> points)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeletePointsForMatchAsync(int matchId)
+        public async Task DeletePointsForMatchAsync(int matchId)
         {
             throw new NotImplementedException();
         }
